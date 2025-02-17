423bebb9a27358dae3bbc1e0114e125c1785b17f
Arjun Khetia <arjun@perceptioncare.com>
2025-02-17 14:10:09 +0530

    Swagger API Documentation

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
 import { UpdateItemDto } from './dto/update-item.dto';
+import { Item } from './entities/item.entity';
 
 @Injectable()
 export class ItemsService {
+  private items: Item[] = [];
+
   create(createItemDto: CreateItemDto) {
-    return 'This action adds a new item';
+    const newItem = { id: this.items.length + 1, ...createItemDto };
+    this.items.push(newItem);
+    return newItem;
   }
 
   findAll() {
-    return `This action returns all items`;
+    return this.items;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} item`;
+    return this.items.find((item) => item.id === id);
   }
 
   update(id: number, updateItemDto: UpdateItemDto) {
-    return `This action updates a #${id} item`;
+    const itemIndex = this.items.findIndex((item) => item.id === id);
+    if (itemIndex === -1) return null;
+    this.items[itemIndex] = { ...this.items[itemIndex], ...updateItemDto };
+    return this.items[itemIndex];
   }
 
   remove(id: number) {
-    return `This action removes a #${id} item`;
+    this.items = this.items.filter((item) => item.id !== id);
+    return { message: 'Item deleted successfully' };
   }
 }
